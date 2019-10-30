67269fbd65aec6a5eb749c4dd70210c226a1d4c1
Chance Penner <chance.penner2016@gmail.com>
2019-10-29 20:32:38 -0500

    create buttons that dissapear once clicked. Add reset button

--- hangman.js
+++ hangman.js
@@ -10,15 +10,16 @@ var hangman =
   getPhrase: ()=>{
     return this.phrase;
   }
-  
+  //add list of all letters, and once used, just remove them. use a function that
+  //just checks "if letter in this array, then get rid of it and then check if in word,
+  //if not in it, then already guessed"
 }
 
  window.setPhrase = function(){
 
-  console.log("YES");
+  console.log("in setPhrase");
   hangman.setPhrase(document.querySelector("#guessPhrase").value);
   console.log(hangman.phrase);
-  console.log("hih");
 
 }
 
@@ -30,3 +31,26 @@ document.getElementById("inputPhrase").addEventListener("submit", function(event
   form.reset();
 
 });
+
+// const buttons = document.querySelectorAll('.letter')
+// buttons.forEach(function(currentBtn){
+//   currentBtn.addEventListener('click', handleEvent)
+// })
+
+var buttons = document.querySelectorAll(".letter");
+var buttonsCount = document.querySelectorAll(".letter").length;
+
+for(let i=0;i<buttonsCount;i++)
+{
+  buttons[i].onclick=function(e){
+    this.style.visibility="hidden"; //makes the letter disappear once guessed
+    //action to be taken once button pushed
+    //alert(this.value);
+  }
+}
+
+for (var i = 0; i < buttons ; i++) {
+    document.querySelectorAll(".letter")[i].addEventListener("click", function() {
+        console.log(document.querySelectorAll(".letter")[i].value)
+    });
+}
